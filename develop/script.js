d994bff183a784c9590c17e529fb78dd306ccbe6
Ricochet227 <skyler7crawford@gmail.com>
2023-10-04 19:26:58 -0600

    Update

--- develop/script.js
+++ develop/script.js
@@ -5,3 +5,31 @@ function initMap() {
       center: location
     });
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+  
+    const searchQuery = document.getElementById('searchQuery').value;
+    const zipCode = document.getElementById('zipCode').value;
+    const radius = document.getElementById('radius').value;
+  
+    // Redirect to search results page with query parameters
+    window.location.href = `search-results.html?searchQuery=${searchQuery}&zipCode=${zipCode}&radius=${radius}`;
+  }
+  function getSearchResults() {
+    const searchQuery = new URLSearchParams(window.location.search).get('searchQuery');
+    const zipCode = new URLSearchParams(window.location.search).get('zipCode');
+    const radius = new URLSearchParams(window.location.search).get('radius');
+  
+    // Use the zip code and radius values in the API request
+    const apiUrl = `https://api.example.com/search?query=${searchQuery}&zipCode=${zipCode}&radius=${radius}`;
+  
+    // Fetch the search results from the API and display them
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Process and display the search results
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
