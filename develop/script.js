0e0daf8cb0022e296f05e73fd3798dc1aa744aa3
Ricochet227 <skyler7crawford@gmail.com>
2023-10-04 19:43:34 -0600

    update

--- develop/script.js
+++ develop/script.js
@@ -1,35 +1,21 @@
 function initMap() {
-    var location = { lat: 40.7128, lng: -74.0060 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: location
-    });
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-  
-    const searchQuery = document.getElementById('searchQuery').value;
-    const zipCode = document.getElementById('zipCode').value;
-    const radius = document.getElementById('radius').value;
-  
-    // Redirect to search results page with query parameters
-    window.location.href = `search-results.html?searchQuery=${searchQuery}&zipCode=${zipCode}&radius=${radius}`;
-  }
-  function getSearchResults() {
-    const searchQuery = new URLSearchParams(window.location.search).get('searchQuery');
-    const zipCode = new URLSearchParams(window.location.search).get('zipCode');
-    const radius = new URLSearchParams(window.location.search).get('radius');
-  
-    // Use the zip code and radius values in the API request
-    const apiUrl = `https://api.example.com/search?query=${searchQuery}&zipCode=${zipCode}&radius=${radius}`;
-  
-    // Fetch the search results from the API and display them
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        // Process and display the search results
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
+  var location = { lat: 40.7128, lng: -74.0060 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: location
+  });
+}
+
+function applyZipCodeAndRadius() {
+  const zipCode = document.getElementById('zipCode').value;
+  const radius = document.getElementById('radius').value;
+  // Use the zip code and radius values to update the map
+  // For example, you can call a function to update the map markers based on the zip code and radius
+  updateMapMarkers(zipCode, radius);
+}
+
+function updateMapMarkers(zipCode, radius) {
+  // Implement your logic to update the map markers based on the zip code and radius
+  // You can use the Google Maps API to perform geocoding and calculate distances
+  // Then, update the map with the new markers
+}
